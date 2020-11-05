01c8c4f412281326e64602d89dbe35727f8881eb
juan <juan.cantero@outlook.com>
2020-11-05 16:07:58 -0300

    add user model

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes/routes.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import colors from 'colors';
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.use(
   cors({
@@ -16,5 +19,7 @@ app.use('/api', routes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`server on ${process.env.NODE_ENV} mode running on port ${PORT}`);
+  console.log(
+    `server on ${process.env.NODE_ENV} mode running on port ${PORT}`.bgBlue.bold
+  );
 });
