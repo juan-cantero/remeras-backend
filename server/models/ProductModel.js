0bfcb11f6769a8bb4bc1e41417500b4a8fcd7a94
juan <juan.cantero@outlook.com>
2020-11-05 16:22:33 -0300

    add product model

---
+++ server/models/ProductModel.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+    forGenre: {
+      type: String,
+      enum: ['hombre', 'mujer', 'unisex'],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
